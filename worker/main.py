ddbc5bee5b639db153ff30fc8153d74f0cebca09
Dario Alvarez <dario.alvarez@vinci-energies.net>
2022-04-20 16:22:06 +0200

    Turn tasks to sync

--- worker/main.py
+++ worker/main.py
@@ -1,5 +1,5 @@
 from celery import Celery
-import asyncio
+import time
 from asgiref.sync import async_to_sync
 
 BROKER_URL = 'redis://localhost:6379/0'
@@ -8,33 +8,17 @@ app = Celery('tasks', broker=BROKER_URL, backend=BACKEND_URL)
 
 
 
-##################################################################
-### LONG TASK (10s)
-##################################################################
-
-async def long_task_async(x, y):
-    await asyncio.sleep(10)
-    return x + y
-
 @app.task(name='add_long')
 def long_task(x, y):
     print('Executing long task...')
-    result = async_to_sync(long_task_async)(x, y)
+    time.sleep(10)
     print('Long task finished!')
-    return f'long task finished: {result}'
-
-
-##################################################################
-### SHORT TASK (1s)
-##################################################################
-
-async def short_task_async(x, y):
-    await asyncio.sleep(1)
     return x + y
+
 
 @app.task(name='add_short')
 def short_task(x, y):
     print('Executing short task...')
-    result = async_to_sync(short_task_async)(x, y)
+    time.sleep(10)
     print('Short task finished!')
-    return f'short task finished: {result}'
+    return x + y
