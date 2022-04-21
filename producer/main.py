11127d75abe02c147345aa2b88ea9fc6467f79b9
Dario Alvarez <dario.alvarez@vinci-energies.net>
2022-04-21 12:49:55 +0200

    Update producer

--- producer/main.py
+++ producer/main.py
@@ -5,6 +5,8 @@ BROKER_URL = 'redis://localhost:6379/0'
 BACKEND_URL = 'redis://localhost:6380/0'
 app = Celery('tasks', broker=BROKER_URL, backend=BACKEND_URL)
 
+
+# Send 3 tasks to Celery
 print('Sending short task...')
 short_result = app.send_task("add_short", (1, 2), apply_async=True)
 print('Sending long task...')
@@ -13,11 +15,10 @@ print('Sending another short task...')
 start = time.time()
 another_short_result = app.send_task("add_short", (5, 6), apply_async=True)
 
-# response = short_result.get()
-# print(f'Response: {response}')
 
+# Wait for the last task sended to finish and check for elapsed time.
+# Check previous tasks do not block the main thread.
 response = another_short_result.get()
-print(f'Last task finished in: {start - time.time() :.2f} seconds')
-
+print(f'Last task finished in: {time.time() - start :.2f} seconds')
 response = long_result.get()
-# print(f'Response: {response}')
+print(f'Response: {response}')
